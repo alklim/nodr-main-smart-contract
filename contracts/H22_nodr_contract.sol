28df650ac0c0351837b7524f1d8ad61006e739ec
Aleksey Klimenko <alex@teleport.media>
2022-05-06 15:28:13 +0500

    Refactor contract 1.0.2

--- contracts/H22_nodr_contract.sol
+++ contracts/H22_nodr_contract.sol
@@ -23,11 +23,11 @@ contract h22_nodr_contract is HederaTokenService {
     uint64  maxTokenSupply;         // Total supply after reward all epochs (equals (epochFrom-epochTo+1)*mintFactor)
     uint64  fraction;               // Fractionality of NDR tokens (how many decimals to count)
     uint256 currentTotalTraffic;    // Current total summary traffic that have been committed to the moment
-    uint64  currentTotalTxCount;    // Current total summary traffic that have been committed to the moment
+    uint64  currentTotalTxCount;    // Current total transactions count that have been committed to the moment
 
-    // temporary variables to test contract (will be removed in production)
-    uint256 treasuryVolume;         // imitate working with NDR treasury TODO: need to mint new NDR on start of each epoch
-    uint256 rewarded;               // total rewarded NDR in a variable  TODO: need to transfer NDR reward to nodr address
+    // temporary variables to test contract
+    uint256 treasuryVolume;
+    uint256 rewarded;
 
     constructor (address _tokenAddress, address _treasuryAddress) public {
         owner = msg.sender;
@@ -42,9 +42,9 @@ contract h22_nodr_contract is HederaTokenService {
         currentTotalTraffic = 0;
         currentTotalTxCount = 0;
 
-        // temporary variables to test contract (will be removed in production)
-        treasuryVolume = fraction*mintFactor;       // initial tokens in treasury
-        rewarded = 0;                               // variable to imitate how much tokens will be transferred to nodrs
+        // temporary variables to test contract
+        treasuryVolume = fraction*mintFactor;
+        rewarded = 0;
     }
 
     function getCurrentStat() public returns (uint256, uint256, uint256, uint256, uint256, uint256, uint256) {
@@ -57,19 +57,15 @@ contract h22_nodr_contract is HederaTokenService {
 
         if (currentTotalTraffic + _amount < (1<<k)) {
             // if adding _amount to traffic sum WILL NOT reach target than we add it to trafficSum and reward _amount
-
-            (bool success) = _reward(_receiver, _amount, false);       // Only if reward is successful we call _trafficAdd
-            require(success);                               // Only if reward is successful we call _trafficAdd
-            _trafficAdd(_amount);                           // Only if reward is successful we call _trafficAdd
-
+            (bool success) = _reward(_receiver, _amount, false);        // Only if reward is successful we call _trafficAdd
+            require(success);                                           // Only if reward is successful we call _trafficAdd
+            _trafficAdd(_amount);                                       // Only if reward is successful we call _trafficAdd
         } else {
             // if adding _amount to traffic sum WILL reach target than we add only part of traffic and reward that part
             uint256 trafficPortion = (1<<k) - currentTotalTraffic;
-
-            (bool success) = _reward(_receiver, trafficPortion, true); // Only if reward is successful we call _trafficAdd
-            require(success);                               // Only if reward is successful we call _trafficAdd
-            _trafficAdd(trafficPortion);                    // Only if reward is successful we call _trafficAdd
-
+            (bool success) = _reward(_receiver, trafficPortion, true);  // Only if reward is successful we call _trafficAdd
+            require(success);                                           // Only if reward is successful we call _trafficAdd
+            _trafficAdd(trafficPortion);                                // Only if reward is successful we call _trafficAdd
             uint256 remainingTraffic = _amount - trafficPortion;
             if (remainingTraffic > 0) trafficCommit(_receiver, remainingTraffic);
         }
@@ -84,44 +80,39 @@ contract h22_nodr_contract is HederaTokenService {
 
     // internal function that rewards nodr account with NDR tokens depending on traffic amount and current epoch
     function _reward (address _receiver, uint256 _amount, bool _epochChg) internal returns (bool) {
+        bool result;
         if (k >= epochFrom && k <= epochTo) {
             uint256 reward = _epochChg ? treasuryVolume : _amount*fraction*mintFactor/(1<<(k-1));
-                                        // TODO: create code for reward nodr account with NDR tokens from treasury
-            rewarded += reward;         // Here we need to transfer reward
-            treasuryVolume -= reward;   // from treasury account to nodr account using
-                                        // transferToken(token, sender, receiver, amount) from HederaTokenService.sol
             int64 value = int64(uint64(reward));
-            _tokenTransfer(tokenAddress, treasuryAddress, _receiver, value);
+            (result) = _tokenTransfer(tokenAddress, treasuryAddress, _receiver, value);
+            if (result) {
+                rewarded += reward;
+                treasuryVolume -= reward;
+            }
         }
         // calling with _epochChg=true will lead to update target traffic and will mint new tokens to treasury
         if (_epochChg) {
-            if (k >= epochFrom && k < epochTo) _mintToTreasury(mintFactor);
+            if (k >= epochFrom && k < epochTo) _tokenMintToTreasury(fraction*mintFactor);
             k++;
         }
-        bool status = true;         // TODO: here we MUST apply the STATUS of token transfer
-        return status;
-    }
-
-    function _mintToTreasury (uint64 _mintAmount) internal {
-                                                // TODO: create code for mint new NDR tokens to treasury
-        treasuryVolume += fraction*_mintAmount; // Here we need to mint new tokens using
-                                                // mintToken(token, amount, metadata) from HederaTokenService.sol
-        _tokenMint(mintFactor*fraction);
+        return result;
     }
 
     // Token transfer function that sends token amount from treasury to nodr account
-    function _tokenTransfer(address _tokenId, address _sender, address _receiver, int64 _amount) internal {
+    function _tokenTransfer(address _tokenId, address _sender, address _receiver, int64 _amount) internal returns (bool) {
         int response = HederaTokenService.transferToken(_tokenId, _sender, _receiver, _amount);
         if (response != HederaResponseCodes.SUCCESS) {
             revert ("Transfer Failed");
         }
+        return true;
     }
 
     // Token mint function that generates new tokens, calls on each epoch change
-    function _tokenMint(uint64 _amount) internal {
+    function _tokenMintToTreasury(uint64 _amount) internal {
         (int response, uint64 newTotalSupply, int64[] memory serialNumbers) = HederaTokenService.mintToken(tokenAddress, _amount, new bytes[](0));
         if (response != HederaResponseCodes.SUCCESS) {
             revert ("Mint Failed");
         }
+        treasuryVolume += _amount;
     }
 }
